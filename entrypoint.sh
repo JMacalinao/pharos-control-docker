1af7002637c09d948704fad1243710b9cdc89303
JJ Macalinao <jessie.macalinao@cai.io>
2021-07-22 00:38:17 +0800

    Added conf directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+if [[ ! -f /opt/pharoscontrol/conf ]]; then
+    cp -R /app/conf/. /opt/pharoscontrol/conf
+fi
 if [[ ! -f /opt/pharoscontrol/db ]]; then
     cp -R /app/db/. /opt/pharoscontrol/db
 fi
+
+chown -R pharoscontrol:pharoscontrol /opt/pharoscontrol/conf
 chown -R pharoscontrol:pharoscontrol /opt/pharoscontrol/db
 exec gosu pharoscontrol "${@}"
