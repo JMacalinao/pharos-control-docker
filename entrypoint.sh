be8b359d01c939b7d50652327859de6badcf23d8
JJ Macalinao <i@jmac.ph>
2022-11-21 12:47:18 +0800

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 if [[ ! -d /app/config/conf ]]; then
-    mv /opt/pharoscontrol/conf /app/config/conf
-    ln -s /app/config/conf /opt/pharoscontrol/conf
+    mv /opt/pharoscontrol/conf /app/config
+else
+    rm -rf /opt/pharoscontrol/conf
 fi
 if [[ ! -d /app/config/db ]]; then
-    mv /opt/pharoscontrol/db /app/config/db
-    ln -s /app/config/db /opt/pharoscontrol/db
+    mv /opt/pharoscontrol/db /app/config
+else
+    rm -rf /opt/pharoscontrol/db
 fi
+
+ln -s /app/config/conf /opt/pharoscontrol/conf
+ln -s /app/config/db /opt/pharoscontrol/db
 
 chown -R pharoscontrol:pharoscontrol /opt/pharoscontrol/conf
 chown -R pharoscontrol:pharoscontrol /opt/pharoscontrol/db
